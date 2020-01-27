14e587de90a75678a47a234cc1480da265ee0b1d
Luke Sathrum <sathrumlu@butte.edu>
2020-01-27 15:20:22 -0800

    untracking

--- promise_examples/1.basic_promise.js
+++
@@ -1,7 +0,0 @@
-// Create the Promise
-const promise = new Promise(function(resolve, reject) {
-  // the function is executed automatically when the promise is constructed
-
-  // after 1 second signal that the job is done with the result "done!"
-  setTimeout(() => resolve("done!"), 1000);
-});
--- promise_examples/2.basic_promise_rejected.js
+++
@@ -1,5 +0,0 @@
-/* Example of rejected promise */
-let promise = new Promise(function(resolve, reject) {
-  // after 1 second signal that the job is finished with an error
-  setTimeout(() => reject(new Error("Whoops!")), 1000);
-});
